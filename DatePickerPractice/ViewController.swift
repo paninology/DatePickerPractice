b2deb57ec403fa4b5ae7f5d91b7266b247ba76be
paninology <paninology@gmail.com>
2022-07-13 15:51:24 +0900

    Layout

--- DatePickerPractice/ViewController.swift
+++ DatePickerPractice/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet var imageViews: [UIImageView]!
+    
+    @IBOutlet var backgroundImages: [UIImageView]!
+    
+    @IBOutlet var dDayLabels: [UILabel]!
+    
+    @IBOutlet var centerLabels: [UILabel]!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        
+       
+        datePicker.preferredDatePickerStyle = .inline
+        
+        
+        
+        
     }
 
 
