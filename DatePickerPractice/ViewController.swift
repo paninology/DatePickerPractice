c342dea59438dc505d26c641e2b86df6a0be225a
paninology <paninology@gmail.com>
2022-07-13 17:19:01 +0900

    images and labels setting

--- DatePickerPractice/ViewController.swift
+++ DatePickerPractice/ViewController.swift
@@ -24,18 +24,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        datePicker.preferredDatePickerStyle = .wheels
-        
-        
        
-        datePicker.preferredDatePickerStyle = .inline
-        
-        
-        
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        }
+        imageSetting()
+        labelSetting()
         
     }
 
+    
+    func imageSetting() {
+        for n in 0...(imageViews.count - 1) {
+            imageViews[n].layer.cornerRadius = 20
+            imageViews[n].clipsToBounds = true
+            backgroundImages[n].layer.cornerRadius = 20
+            backgroundImages[n].clipsToBounds = true
+            backgroundImages[n].alpha = 0.3
+        }
+    }
+    
+    func labelSetting() {
+        for n in 0...(dDayLabels.count - 1) {
+            dDayLabels[n].textColor = .white
+            centerLabels[n].textColor = .white
+            dDayLabels[n].font = .preferredFont(forTextStyle: .headline)
+        }
+    }
+    
+    
+
+//    func datePickerStyleSet() {
+//  if #available(iOS 14.0, *) {
+//    datePicker.preferredDatePickerStyle = .inline
+//}
+//    }
 
 }
 
