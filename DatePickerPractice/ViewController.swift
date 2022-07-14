516c4075621fdc666199d26f800c7807bb6074f3
paninology <paninology@gmail.com>
2022-07-14 22:55:54 +0900

    Added time interval for D-Day calcuation

--- DatePickerPractice/ViewController.swift
+++ DatePickerPractice/ViewController.swift
@@ -10,17 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
-    
     @IBOutlet var imageViews: [UIImageView]!
-    
     @IBOutlet var backgroundImages: [UIImageView]!
-    
     @IBOutlet var dDayLabels: [UILabel]!
-    
     @IBOutlet var centerLabels: [UILabel]!
     
-    
-    
+    var dateForDDay = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,14 +26,20 @@ class ViewController: UIViewController {
         imageSetting()
         labelSetting()
         
+        var dateFormat = DateFormatter()
+        dateFormat.dateFormat = "yyyy/MM/dd"
+        let word = "2019/12/10"  //위와 같아야댐 형식
+        let dateResult = dateFormat.date(from: word)
+        print(dateResult)
+        
+        
         if #available(iOS 15.0, *) {
             dateCal()
         } else {
-            // Fallback on earlier versions
+            //dateCal 15이전 버전 가능하게 수정할것.
         }
         
     }
-
     
     func imageSetting() {
         for n in 0...(imageViews.count - 1) {
@@ -64,17 +65,23 @@ class ViewController: UIViewController {
     func dateCal() {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy년 \n MM월 dd일"
-        
         centerLabels[0].text =
-        formatter.string(from: Date())
-        
+        formatter.string(from: dateForDDay.addingTimeInterval(3600*24*100))
 //        Date().formatted(date: .abbreviated, time: .omitted)
-        
-        centerLabels[1].text = Date().addingTimeInterval(60*60*24*100).formatted(date: .complete, time: .shortened)
-        centerLabels[2].text = Date().addingTimeInterval(3600*24*200).formatted(date: .long, time: .omitted)
-        centerLabels[3].text = Date().addingTimeInterval(3600*24*300).formatted(date: .numeric, time: .standard)
+        centerLabels[1].text = dateForDDay.addingTimeInterval(60*60*24*200).formatted(date: .complete, time: .shortened)
+        centerLabels[2].text = dateForDDay.addingTimeInterval(3600*24*300).formatted(date: .long, time: .omitted)
+        centerLabels[3].text = dateForDDay.addingTimeInterval(3600*24*400).formatted(date: .numeric, time: .standard)
     }
     
+    @IBAction func dateValueChanged(_ sender: UIDatePicker) {
+        
+        dateForDDay = sender.date
+        if #available(iOS 15.0, *) {
+            dateCal()
+        } else {
+            
+        }
+    }
     
 
 //    func datePickerStyleSet() {
